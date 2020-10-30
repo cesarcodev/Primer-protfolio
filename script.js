8129463475c500ec86ca58e9a0e8bb81c7c371b1
Cesar <cesarce735@gmail.com>
2020-10-29 23:56:04 -0400

    barra de navegación v 0.1

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./sw.js')
-    .then(reg => console.log('Registro de SW exitoso', reg))
-    .catch(err => console.warn('Error al tratar de registrar el sw', err))
-}
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker.register('./sw.js')
+//     .then(reg => console.log('Registro de SW exitoso', reg))
+//     .catch(err => console.warn('Error al tratar de registrar el sw', err))
+// }
+
+
+$(document).ready(function(){
+	$(".nav_boton").on('click', function(event) {
+		event.preventDefault();
+		$("#navegacion").animate({
+        height: "toggle",
+      });
+
+      	if ($('#equis').is(':visible')) {
+		    $('#equis').hide();
+		    $('#barra').show();
+		} else {
+		    $('#equis').show();
+		    $('#barra').hide();
+		}
+	});
+});
