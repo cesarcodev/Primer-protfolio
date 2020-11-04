718b2b68331e2ae360f90c698e688ddf431ab3c3
Cesar <cesarce735@gmail.com>
2020-11-04 00:05:46 -0400

    loaber v 0.1

--- script.js
+++ script.js
@@ -21,3 +21,8 @@ $(document).ready(function(){
 		}
 	});
 });
+
+// window.addEventListener("load", function(){
+// 	const loader = document.querySelector(".loader");
+// 	loader.className += " hidden";
+// });
