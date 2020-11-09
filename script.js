37eab959055e4fa9d7e2b7da2539c4634a2d9081
Cesar <cesarce735@gmail.com>
2020-11-09 18:49:24 -0400

    contacto y footer

--- script.js
+++ script.js
@@ -17,11 +17,46 @@ $(document).ready(function(){
 	});
 });
 
-// window.addEventListener("load", function(){
-// 	const loader = document.querySelector(".loader");
-// 	loader.className += " hiddenLoader";
-// });
+window.addEventListener("load", function(){
+	const loader = document.querySelector(".loader");
+	loader.className += " hiddenLoader";
+});
 
 
+$(function() {
+
+  var $form = $('#contact-me');
+  $form.on("submit", function(e) {
+    var formUrl = $(this).attr('action');
+
+    // We don't want the page to submit and refresh
+    e.preventDefault();
+
+    // POST the request to Formbackend (the formUrl value)
+    $.ajax({
+      url: formUrl,
+      type: 'POST',
+      headers: {
+        'accept': 'application/json',
+      },
+      // We serialize the form fields
+      data: $form.serialize()
+    }).done(function(response) {
+      // var successElm = $('<div class="bg-green-100 border border-green-200 text-green-500 p-3">');
+      // successElm.html(`${response.submission_text}, ${response.values.name}!`);
+
+      // $form.parent().replaceWith(successElm);
+      // console.log("enviado");
+      $('#aviso_bien').show();
+      $form[0].reset();
+    }).fail(function() {
+      // Happens when something goes wrong
+      $('#aviso_error').show();
+    }).always(function() {
+      // This always happens
+    });
+
+  });
+});
 
 
